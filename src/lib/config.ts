e1b892f888ae160663c81ec0a11bf71687949321
Marvin van Aalst <marvin.vanaalst@gmail.com>
2026-06-05 10:24:06 +0200

    fix: unify config

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -4,4 +4,4 @@ export const title = "ComPhot";
 export const description = "Interactive PS learning platform";
 export const url = dev
   ? "http://localhost:5173/"
-  : "https://https://computational-biology-aachen.github.io/comphot/";
+  : "https://computational-biology-aachen.github.io/comphot/";
